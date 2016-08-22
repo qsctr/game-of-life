995356ce07d57f712cf79d8563f498391f05c9d3
qsctr <brettonchen@gmail.com>
2016-08-23 00:03:31 +0800

    Change List to Set to increase speed

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,14 @@
 
 module Main where
 
-import Data.List
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 
-data World = World { cells :: [(Int, Int)], playing :: Bool, viewPort :: ViewPort }
+data World = World { cells :: Set (Int, Int), playing :: Bool, viewPort :: ViewPort }
 
 main :: IO ()
 main = do
@@ -23,31 +25,32 @@ main = do
         , "R: Restart game"
         , "Press enter to continue." ]
     _ <- getLine
-    play (InWindow "Game of Life" (800, 600) (0, 0)) white 5 initialWorld drawWorld handleInput stepWorld
+    play (InWindow "Game of Life" (800, 600) (0, 0)) white 10 initialWorld drawWorld handleInput $ const stepWorld
 
 initialWorld :: World
-initialWorld = World { cells = [], playing = False, viewPort = initialViewPort }
+initialWorld = World { cells = Set.empty, playing = False, viewPort = initialViewPort }
 
 initialViewPort :: ViewPort
-initialViewPort = viewPortInit { viewPortScale = 15 }
+initialViewPort = viewPortInit { viewPortScale = 10 }
 
 drawWorld :: World -> Picture
-drawWorld World {..} = applyViewPortToPicture viewPort $ pictures $ map drawCell cells
+drawWorld World {..} = applyViewPortToPicture viewPort $ pictures $ map drawCell $ Set.toList cells
     where drawCell (x, y) = translate (fromIntegral x) (fromIntegral y) $ rectangleSolid 1 1
 
-stepWorld :: Float -> World -> World
-stepWorld _ world@World { playing = False } = world
-stepWorld _ world@World {..} = world { cells = nub $ concatMap (filter alive . surrounding) cells }
-    where surrounding (x, y) = [ (x + w, y + h) | w <- [-1..1], h <- [-1..1] ]
-          alive cell = if neighbors == 4 then cell `elem` cells else neighbors == 3
-              where neighbors = length $ filter (`elem` cells) $ surrounding cell
+stepWorld :: World -> World
+stepWorld world@World { playing = False } = world
+stepWorld world@World {..} = world { cells = Set.foldr' accFunc Set.empty cells }
+    where accFunc cell acc = Set.union acc $ Set.filter alive $ surrounding cell
+          surrounding (x, y) = Set.fromList [ (x + w, y + h) | w <- [-1..1], h <- [-1..1] ]
+          alive cell = if neighbors == 4 then Set.member cell cells else neighbors == 3
+              where neighbors = Set.size $ Set.filter (`Set.member` cells) $ surrounding cell
 
 handleInput :: Event -> World -> World
 handleInput (EventKey (SpecialKey KeySpace) Down _ _) world@World {..} = world { playing = not playing }
 handleInput (EventKey (MouseButton button) Down _ coords) world@World {..}
-    | button == LeftButton && cell `notElem` cells = world { cells = cell : cells }
-    | button == RightButton && cell `elem` cells = world { cells = delete cell cells }
-    where cell = mapPair round $ invertViewPort viewPort coords
+    | button == LeftButton && Set.notMember cell cells = world { cells = Set.insert cell cells }
+    | button == RightButton && Set.member cell cells = world { cells = Set.delete cell cells }
+    where cell = let (x, y) = invertViewPort viewPort coords in (round x, round y)
 handleInput (EventKey (SpecialKey key) Down _ _) world@World {..}
     | key == KeyLeft = t (tx + 5, ty)
     | key == KeyRight = t (tx - 5, ty)
@@ -63,6 +66,3 @@ handleInput (EventKey (Char c) Down _ _) world@World {..}
 handleInput (EventKey (Char 'c') Down _ _) world = world { viewPort = initialViewPort }
 handleInput (EventKey (Char 'r') Down _ _) _ = initialWorld
 handleInput _ world = world
-
-mapPair :: (a -> b) -> (a, a) -> (b, b)
-mapPair f (x, y) = (f x, f y)
